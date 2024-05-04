7041283688457a23aa03bf51d74dbb56d72e2dc4
jd <jean.daniel.michaud@gmail.com>
2024-05-04 21:46:42 +0200

    test: add test harness

---
+++ tests/quakec/test.sh
@@ -0,0 +1,53 @@
+#!/usr/bin/env bash
+
+if [ $# -ne 1 ]
+then
+  echo "error: expecting the path to qvm binary"
+  exit 1
+fi
+qvm=$1
+
+executed=0
+failed=0
+
+tmpfile1=$(mktemp)
+tmpfile2=$(mktemp)
+
+for testfile in $(ls *.test)
+do
+  fail=0
+  executed=$((executed+1))
+  cmd=$(grep cmd $testfile | awk -F':' '{ print $2 }')
+  exitcode=$(grep exit $testfile | awk -F':' '{ print $2 }')
+  $qvm $cmd > $tmpfile1 2>&1
+  tail +4 $testfile > $tmpfile2  
+  observed=$?
+  echo -n $cmd
+  if [ $observed -ne $exitcode ]
+  then
+    fail=1
+    echo -n " incorrect exit code"
+  fi
+
+  diff $tmpfile1 $tmpfile2
+  if [ $? -ne 0 ]
+  then
+    fail=1
+    echo -n " incorrect output"
+  fi
+
+  if [ $fail -ne 0 ]
+  then
+    failed=$((failed+1))
+    echo ""
+  else
+    echo " OK"
+  fi
+done
+
+echo "$executed executed $failed failed"
+
+if [ $failed -gt 0 ]
+then
+  exit 1
+fi
